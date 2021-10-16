3cd4e449cb11a6a031a2467dfdde8c8d02196318
RowanNewell <rowan@rowannewell.com>
2021-10-17 09:34:54 +1000

    addition and subtraction were pretty straight forward. used 'arguments' to be able to process any number of parameters passed through. Because they're testing with arrays for sum and multiplication, had to put that parameter in. could refactor to determine if first paramater is array or not and calculate from there

--- Exercises/08_calculator/calculator.js
+++ Exercises/08_calculator/calculator.js
@@ -1,17 +1,33 @@
 const add = function() {
-	
+  let addition = 0;
+    for (i = 0; i < arguments.length; i++){
+    addition = addition + arguments[i];
+  } 
+  return addition;
 };
 
 const subtract = function() {
-	
+  let sub = arguments[0];
+    for (i = 1; i < arguments.length; i++){
+    sub = sub - arguments[i];
+  } 
+  return sub;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+	let sumVal = 0;
+    for (i = 0; i < sumArray.length; i++){
+      sumVal = sumVal + sumArray[i];
+  } 
+  return sumVal;
 };
 
-const multiply = function() {
-
+const multiply = function(multArray) {
+  let mult = 1;
+    for (i = 0; i < multArray.length; i++){
+      mult = mult * multArray[i];
+  } 
+  return mult;
 };
 
 const power = function() {
