2509feffe451b6ad44acbc9bf37d0dfb881c1c8a
RowanNewell <rowan@rowannewell.com>
2021-10-17 09:44:52 +1000

    had to google the factorial function. Was getting call stack size error as the recursive function didn't have a base case that could be met. adding in the if statements resolved the error

--- Exercises/08_calculator/calculator.js
+++ Exercises/08_calculator/calculator.js
@@ -30,12 +30,18 @@ const multiply = function(multArray) {
   return mult;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return Math.pow(x,y);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0) 
+        return -1;
+  else if (num == 0) 
+      return 1;
+  else {
+      return (num * factorial(num - 1));
+  }
 };
 
 // Do not edit below this line
