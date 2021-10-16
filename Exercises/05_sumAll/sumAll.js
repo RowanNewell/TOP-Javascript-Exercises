aa2c6a52354b5cde5be1137b812575c8744ba206
RowanNewell <rowan@rowannewell.com>
2021-10-16 16:40:48 +1000

    had to google this one. Math.min and Math.max was news to me and glad I found them.

--- Exercises/05_sumAll/sumAll.js
+++ Exercises/05_sumAll/sumAll.js
@@ -1,17 +1,9 @@
 const sumAll = function(num1, num2) {
 
-    // if(num1 < num2){
-    //     let low = num1;
-    //     let high = num2;
-    // } else {
-    //     let low = num2;
-    //     let high = num1;
-    // }
-
     let sum = 0;
-    let low = num1;
-    let high = num2;
-    for (i = low; i <= high; i++){
+    let lowNum = Math.min(num1,num2);
+    let highNum = Math.max(num1,num2);
+    for (i = lowNum; i <= highNum; i++){
         sum += i;
     }
  return sum;
