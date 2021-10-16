162accf2b5e606138496cd1b27690cfd13457c22
RowanNewell <rowan@rowannewell.com>
2021-10-16 16:27:17 +1000

    sumAll works while first number is less than second number.. Need to compare numbers and assign highest to 'high' variable.

--- Exercises/05_sumAll/sumAll.js
+++ Exercises/05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    // if(num1 < num2){
+    //     let low = num1;
+    //     let high = num2;
+    // } else {
+    //     let low = num2;
+    //     let high = num1;
+    // }
+
+    let sum = 0;
+    let low = num1;
+    let high = num2;
+    for (i = low; i <= high; i++){
+        sum += i;
+    }
+ return sum;
 };
 
 // Do not edit below this line
