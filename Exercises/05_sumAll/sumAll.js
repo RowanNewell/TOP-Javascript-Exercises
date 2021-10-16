e1d0b01609aebd71625e514c07bcace42c440cd8
RowanNewell <rowan@rowannewell.com>
2021-10-16 16:56:35 +1000

    Worked through the rest of the tests. was trying to use typeof if statement after numbers were assigned to sum, but variables were all typeof number. Wrapped other functions in qualifier if statement first.

--- Exercises/05_sumAll/sumAll.js
+++ Exercises/05_sumAll/sumAll.js
@@ -1,12 +1,20 @@
 const sumAll = function(num1, num2) {
-
-    let sum = 0;
-    let lowNum = Math.min(num1,num2);
-    let highNum = Math.max(num1,num2);
-    for (i = lowNum; i <= highNum; i++){
-        sum += i;
+    if (typeof num1 === "number" & typeof num2 === "number"){
+        let sum = 0;
+        let lowNum = Math.min(num1,num2);
+        let highNum = Math.max(num1,num2);
+        for (i = lowNum; i <= highNum; i++){
+            sum += i;
+        }
+        if (sum < 0){
+            return 'ERROR';
+        } else {
+            return sum;
+        
+        }
+    } else {
+        return 'ERROR';
     }
- return sum;
 };
 
 // Do not edit below this line
