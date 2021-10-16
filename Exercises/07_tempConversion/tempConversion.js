4e62105b53faf377d8bbfa219dc29410bc5cd311
RowanNewell <rowan@rowannewell.com>
2021-10-17 08:56:16 +1000

    quick google on how to round to one decimal place got this one sorted pretty easily

--- Exercises/07_tempConversion/tempConversion.js
+++ Exercises/07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
+const round = function(number){
+    number = Math.round(number * 10)/10;
+    return number
+}
 
+const ftoc = function(farenheittoc) {
+    const ftocelsius = (farenheittoc - 32) * (5/9);
+    return round(ftocelsius);
 };
 
-const ctof = function() {
-
+const ctof = function(celsiustof) {
+    const ctofarenheit = celsiustof *(9/5) + 32;
+    return round(ctofarenheit);
 };
 
 // Do not edit below this line
